1ef5c1cbc68e84dc23400a4e960f947660ae85e9
HOU XINYUAN <2489622106@qq.com>
2025-11-18 12:24:53 +0800

    enable prepare dataset.py be used in multi dataset:coco and flickr

--- prepare_dataset.py
+++ prepare_dataset.py
@@ -6,10 +6,6 @@ import json
 
 if __name__ == "__main__":
     # #load dataset
-    # dataset = load_dataset(
-    #     "lmms-lab/COCO-Caption2017",
-    #     split="val",
-    # )
     dataset= load_dataset("Adyakanta/test_flickr30k",split="test")
     #prepare output folder
     output_dir = "data/flickr"
@@ -37,4 +33,40 @@ if __name__ == "__main__":
     with open(os.path.join(output_dir, "metadata.json"), "w", encoding="utf-8") as f:
         json.dump(metadata, f, indent=2, ensure_ascii=False)
     
-    print("Dataset preparation completed.")
+    print("Dataset flickr30k preparation completed.")
+
+
+
+    #load dataset
+    dataset = load_dataset(
+        "lmms-lab/COCO-Caption2017",
+        split="val",
+    )
+
+    #prepare output folder
+    output_dir = "data/coco"
+    os.makedirs(output_dir, exist_ok=True)
+    
+    #save images and captions
+    metadata = []
+    for i, item in enumerate(dataset):
+        id = item['file_name']
+        image: Image.Image = item["image"]
+        img_path = os.path.join(output_dir, id)
+        image.save(img_path, format="JPEG")
+
+        caption = item["answer"]
+        metadata.append({
+            "id": id,
+            "caption": caption
+        })
+
+        if i%500 == 0:
+            print(f"Saved {i} samples...")
+    
+    # Save metadata JSON
+    with open(os.path.join(output_dir, "metadata.json"), "w", encoding="utf-8") as f:
+        json.dump(metadata, f, indent=2, ensure_ascii=False)
+    
+    print("Dataset coco preparation completed.")
+
